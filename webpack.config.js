e3282c39583e1d4c3afb8d176a6d5d67db7d2c07
Gavin Dibley <gavin@gavindibley.id.au>
2020-05-24 09:40:34 +1000

    Round 2 using serverless appand a few tweaks to folder structure

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+// webpack.config.js
+const path = require("path");
+const slsw = require("serverless-webpack");
+const nodeExternals = require("webpack-node-externals");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+module.exports = {
+  entry: slsw.lib.entries,
+  target: "node",
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
+  optimization: {
+    // We no not want to minimize our code.
+    minimize: false
+  },
+  performance: {
+    // Turn off size warnings for entry points
+    hints: false
+  },
+  devtool: "nosources-source-map",
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        include: __dirname,
+        exclude: /node_modules/,
+        
+        query: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["css-modules-transform"]
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+            'style-loader',
+            {
+                loader: 'css-loader',
+                options: {modules: true, sourceMap: true}
+            }
+        ]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8000,
+              name: "images/[hash]-[name].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin(
+       { patterns: [{ from: "client/build", to: "client/build" }]}, {
+      debug: "info"
+    })
+  ]
+};
